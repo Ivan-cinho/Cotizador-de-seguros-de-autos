eabf1a4980837cadee53d6caa3e3d2a15ac01c0e
ivan.cinho <pabloivan84@gmail.com>
2023-05-13 14:02:20 -0300

    13-5

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 const selectorDeAño = document.querySelector("#selectorDeAño")
 const selectorDeMarca = document.querySelector("#selectorDeMarca")
+const selectorDeGama = document.querySelector("#selectorDeGama")
