bf11ed02ce3e692651dc5d9591c9c7044a944431
ivan.cinho <pabloivan84@gmail.com>
2023-06-10 13:51:53 -0300

    10/6

--- index.js
+++ index.js
@@ -44,15 +44,104 @@ function validarDatosCompletos() {
     return (selectorDeAño.value !== "Selecciona el año" && selectorDeMarca.value !== "Selecciona la marca" && selectorDeGama.value !== "Selecciona la gama")
 }
 
+
 function realizarCotizacion() {
     if (validarDatosCompletos()) {
         const cotizo = new Cotizador(costoBase, selectorDeAño.value, selectorDeMarca.value, selectorDeGama.value, valorGnc(), valorComercial())
-        alert("El valor de la cuota de tu seguro es de $" + parseInt(cotizo.cotizar()))
+        swal.fire({
+            width: '70%',
+            backdrop: 'true',
+            title: 'El valor estimado de la cuota de tu seguro es de $' + parseInt(cotizo.cotizar()),
+            imageUrl: 'imagenes/lacaja.svg',
+            imageWidth: '7%',
+            imageAlt: 'La caja',
+            showCancelButton: true,
+            confirmButtonText: 'Medios de pago',
+            cancelButtonText: 'Quiero que me contacten',
+            // html: '<button class="btn btn-danger">Enviar cotizacion por mail</button><button class="btn btn-danger">medios de pago</button>'
+        }) .then((r) => {
+            if (r.isConfirmed) {
+                mostrarMediosDePago()
+            } else {
+                quieroQueMeContacten()
+            }
+        }) 
     } else {
-        alert("completa todo che!")
+        llenarDatos()
     }
 }
 
+
 btnCotizacion.addEventListener("click", ()=> {
     realizarCotizacion()
+})
+
+
+const llenarDatos = () => {
+    swal.fire({
+        icon: 'error',
+        width: '70%',
+        backdrop: 'true',
+        title: 'Completar los datos para realizar la cotizacion'
+    })
+}
+
+const quieroQueMeContacten = () => {
+    Swal.fire ({
+        width: '50%',
+        imageUrl: 'imagenes/lacaja.svg',
+        imageWidth: '7%',
+        confirmButtonText: 'Enviar',
+        title: 'Ingrese sus datos y sera contactado a la brevedad',
+        html:   '<input class="form-control form-control-sm formQuieroQueMeContacten" type="text" placeholder="Nombre y apellido" aria-label=".form-control-lg">'+
+                '<input class="form-control form-control-sm formQuieroQueMeContacten" type="text" placeholder="Correo electronico" aria-label=".form-control-lg">'+
+                '<textarea name="" class="form-control textAreaQuieroQueMeContacten" cols="20" rows="10" placeholder="Escriba su inquietud"></textarea>'
+    })
+}
+
+// medios de pago y seguridad
+
+const btnMediosDePago = document.getElementById("mediosDePago")
+const btnSitioSeguro = document.getElementById("sitioSeguro")
+
+const mostrarMediosDePago = () => {
+    swal.fire({
+        title: '¿Cómo puedo pagar mi seguro?',
+        width: '50%',
+        backdrop: 'true',
+        imageUrl: 'imagenes/lacaja.svg',
+        imageWidth: '7%',
+        imageAlt: 'La caja',
+        showConfirmButton: false,
+        html: '<hr class="hr1">'+
+        '<h4>Tarjetas de credito</h4>'+
+        '<img class="tarjeta" src="imagenes/visa.svg" alt="">'+
+        '<img class="tarjeta naranja" src="imagenes/naranja.svg" alt="">'+
+        '<img class="tarjeta" src="imagenes/mastercard.svg" alt="">'+
+        '<hr class="hr1">'+
+        '<h4>Mercado pago</h4>'+
+        '<img class="tarjeta" src="imagenes/mercadopago.svg" alt="">'+
+        '<hr class="hr1">'+
+        '<h4>Efectivo</h4>'+
+        '<img class="tarjeta" src="imagenes/pagofacil.svg" alt="">'+
+        '<img class="tarjeta pagoMisCuentas" src="imagenes/pagomiscuentas.svg" alt="">'+
+        '<img class="tarjeta rapipago" src="imagenes/rapipago.svg" alt="">',
+    })
+}
+
+const mostrarSitioSeguro = () => {
+    swal.fire({
+        showConfirmButton: false,
+        html: '<h3 class="tituloSitioSeguro">Sitio 100% seguro</h3>'+
+        '<p>Cumplimos con los más altos estándares de seguridad de la industria</p>'+
+        '<p>Todos los pagos de La Caja se procesan en nuestra propia plataforma para pagar de forma segura. Por eso, no importa cómo pagues, ¡tu dinero siempre está protegido!</p>',
+    })
+}
+
+btnMediosDePago.addEventListener("click", ()=> {
+    mostrarMediosDePago()
+})
+
+btnSitioSeguro.addEventListener("click", ()=> {
+    mostrarSitioSeguro()
 })
